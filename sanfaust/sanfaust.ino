0e2cdf33218c6a2da69d62ed24a36769cd2fb315
Andrea Stagi <stagi.andrea@gmail.com>
2016-02-14 15:21:54 +0100

    New animations

--- sanfaust/sanfaust.ino
+++ sanfaust/sanfaust.ino
@@ -10,15 +10,27 @@
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 uint8_t HEART_BOOM[] = {
-  0x20, HEART_BOOM_1_ID, HEART_BOOM_2_ID, HEART_EMPTY_ID, 
+  0x20, HEART_BOOM_1_ID, HEART_BOOM_2_ID, HEART_EMPTY_ID,
   HEART_FILLED_ID, HEART_EMPTY_ID, HEART_FILLED_ID,  HEART_EMPTY_ID, 
   HEART_FILLED_ID,  HEART_EMPTY_ID, HEART_BOOM_2_ID, HEART_BOOM_1_ID, 0x20
 };
 
-Animation *heart_boom_anim_1 = new LoopAnimation(0, 13, HEART_BOOM, sizeof(HEART_BOOM));
+uint8_t HEART_BOOM_FROM_BOTTOM[] = {
+  0x20, HEART_BOTTOM_2_ID, HEART_BOTTOM_1_ID, HEART_EMPTY_ID,
+  HEART_FILLED_ID, HEART_EMPTY_ID, HEART_FILLED_ID,  HEART_EMPTY_ID, 
+  HEART_FILLED_ID,  HEART_EMPTY_ID, HEART_BOTTOM_1_ID, HEART_BOTTOM_2_ID, 0x20
+};
+
+uint8_t HEART_BOOM_FROM_TOP[] = {
+  0x20, HEART_TOP_1_ID, HEART_TOP_2_ID, HEART_EMPTY_ID,
+  HEART_FILLED_ID, HEART_EMPTY_ID, HEART_FILLED_ID,  HEART_EMPTY_ID, 
+  HEART_FILLED_ID,  HEART_EMPTY_ID, HEART_TOP_2_ID, HEART_TOP_1_ID, 0x20
+};
+
+Animation *heart_boom_anim_1 = new LoopAnimation(0, 13, HEART_BOOM_FROM_TOP, sizeof(HEART_BOOM_FROM_TOP));
 Animation *heart_boom_anim_2 = new LoopAnimation(0, 2, HEART_BOOM, sizeof(HEART_BOOM));
 Animation *heart_boom_anim_3 = new LoopAnimation(1, 14, HEART_BOOM, sizeof(HEART_BOOM));
-Animation *heart_boom_anim_4 = new LoopAnimation(1, 1, HEART_BOOM, sizeof(HEART_BOOM));
+Animation *heart_boom_anim_4 = new LoopAnimation(1, 1, HEART_BOOM_FROM_BOTTOM, sizeof(HEART_BOOM_FROM_BOTTOM));
 Animation *heart_boom_anim_5 = new LoopAnimation(0, 0, HEART_BOOM, sizeof(HEART_BOOM));
 Animation *heart_boom_anim_6 = new LoopAnimation(0, 15, HEART_BOOM, sizeof(HEART_BOOM));
 
@@ -26,7 +38,7 @@ Animation *single_anim_1 = new BlinkTextAnimation(0, 6, "I'M", 4);
 Animation *single_anim_2 = new BlinkTextAnimation(1, 4, "SINGLE!!", 4);
 
 Animation *my_number_is_anim_1 = new TextAnimation(0, 6, "MY", 4);
-Animation *my_number_is_anim_2 = new TextAnimation(1, 3, "NUMBER IS", 4);
+Animation *my_number_is_anim_2 = new TextAnimation(1, 3, "NUMBER IS:", 4);
 
 Animation *my_number_anim_1 = new ProgressTextAnimation(0, 6, "339");
 Animation *my_number_anim_2 = new ProgressTextAnimation(1, 4, "2625610");
